8fe88f6055ccd9cd0d53f3c94c2afc6ca2295687
Vitaliy Chiley <vitaliy@cerebras.net>
2020-03-25 14:21:34 -0700

    Add actClamp and Combine BON and ON (#5)
    * add actClamp as ECM, combine BON and ON into one ON since its one algorithm.
    
    * updt

--- online-norm/tensorflow/online_norm_tf/__init__.py
+++ online-norm/tensorflow/online_norm_tf/__init__.py
@@ -1,7 +1,6 @@
 """
-Released under BSD 3-Clause License, 
+Released under BSD 3-Clause License,
 Copyright (c) 2019 Cerebras Systems Inc.
 All rights reserved.
 """
 from .online_norm import OnlineNorm, online_norm
-from .batch_online_norm import BatchOnlineNorm, batch_online_norm
