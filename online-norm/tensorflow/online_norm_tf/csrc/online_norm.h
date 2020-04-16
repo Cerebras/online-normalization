e9a68cc9488b23b44e0fdb945fea824f07bba66a
Vitaliy Chiley <vitaliy@cerebras.net>
2020-04-15 23:52:40 -0700

    update kernel

--- online-norm/tensorflow/online_norm_tf/csrc/online_norm.h
+++ online-norm/tensorflow/online_norm_tf/csrc/online_norm.h
@@ -8,10 +8,9 @@ using namespace tensorflow;
 using CPUDevice = Eigen::ThreadPoolDevice;
 using GPUDevice = Eigen::GpuDevice;
 
-// 3 kernels:
+// 2 kernels:
 // 1. norm_fwd_kernel
-// 2. norm_uctrl_kernel
-// 3. norm_vctrl_kernel
+// 2. norm_bwd_kernel
 
 template <typename Device, typename T>
 struct OnlineNormFwdFunctor {
@@ -32,29 +31,19 @@ struct OnlineNormFwdFunctor {
   );
 };
 
-template <typename Device, typename T>
-struct OnlineNormUCtrlFunctor {
-  void operator()(
-    const Device& d,
-    const float* mu_delta, // [N, C]
-    const float* in_u, // [C]
-    float* out_u, // [C]
-    T* d_u,  // [N, C]
-    const int C,
-    const int N,
-    const float abkw
-  );
-};
 
 template <typename Device, typename T>
-struct OnlineNormVCtrlFunctor {
+struct OnlineNormBwdFunctor {
   void operator()(
     const Device& d,
-    const T* grad_outputs, // [N, C, D]
-    const T* outputs, // [N, C, D]
-    const float* in_v, // [C]
-    float* out_v, // [C]
-    T* grad_tmp, // [N, C, D]
+    const T* grad_outputs,  // [N, C, D]
+    const float* in_v,      // [C]
+    const float* in_u,      // [C]
+    const T* outputs,       // [N, C, D]
+    const T* scale,         // [N, C]
+    float* out_v,           // [C]
+    float* out_u,           // [C]
+    T* grad_in,             // [N, C, D]
     const int C,
     const int N,
     const int D,
